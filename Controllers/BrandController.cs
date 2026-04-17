40d8741750516fc3bb7e0b0cedb0783d88913941
ankitchauhan1221 <ankitchauhan1221@gmail.com>
2026-04-17 13:19:16 +0530

    kjhyuiokmnhbho

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,31 @@
+﻿using firstproject.Models.BusinessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace firstproject.Controllers
+{
+
+    [ApiController]
+    [Route("api/admin")]
+    public class BrandController : ControllerBase
+    {
+        private readonly IBusinessLayer _businessLayer;
+
+        public BrandController(IBusinessLayer businessLayer)
+        {
+            _businessLayer = businessLayer;
+        }   
+
+           [HttpGet]
+            [Route("getbrand")]
+            public async Task<IActionResult> Get()
+            {
+                var result = await _businessLayer.GetBrand();
+                return Ok(result);
+
+             }
+
+
+
+
+    }
+}
